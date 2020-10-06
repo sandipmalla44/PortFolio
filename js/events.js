698d39f07d59a305c49138654bd35e07d3abccfe
sandi <sandi@DESKTOP-0CP5J4Q>
2020-10-06 17:52:55 +0545

    footer and box

---
+++ js/events.js
@@ -0,0 +1,16 @@
+
+// $(document).ready(function () {  
+//     $('.blog-Container .blog-block').attr('id','blog-button');
+// });
+
+// // const blogButton = document.getElementById('blog-button');
+// // blogButton.addEventListener('click',function () {  
+
+// // })
+
+// $('blog-button').append('<a href="'+ www.dokocrunch.com + '"/>');
+
+// $('blog-block').click(function () { 
+//     e.preventDefault();
+    
+// });
