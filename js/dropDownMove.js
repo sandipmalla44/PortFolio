c9ed46f5d331360710b5b51ae9da2306ef615447
sandi <sandipmalla44@gmail.com>
2020-11-19 15:59:01 +0545

    updated

--- js/dropDownMove.js
+++ js/dropDownMove.js
@@ -7,11 +7,9 @@ const button = document.getElementById('dropDownButton');
 $(document).ready(function () {
     $(window).scroll(function () {
         if ($(document).scrollTop() > 50){
-            $("nav").addClass("scrolled");
             $("nav").addClass("scrolled-down");
             } 
         else {
-            $("nav").removeClass("scrolled");
             $("nav").removeClass("scrolled-down");
         }
         if ($(document).scrollTop() > 300) {
@@ -21,6 +19,36 @@ $(document).ready(function () {
         else {
             $(".nav-link").removeClass("color-a");
             $("nav").removeClass("scrolled-up");
+        }
+    });
+});
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > 750){
+            console.log("Yeah");
+            $(".card1").addClass("scroll-left");
+        } 
+        else {
+            $(".card1").removeClass("scroll-left");
+        }
+    });
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > 870){
+            console.log("Yeah 2");
+            $(".card2").addClass("scroll-left");
+        } 
+        else {
+            $(".card2").removeClass("scroll-left");
+        }
+    });
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > 990){
+            console.log("Yeah 3");
+            $(".card3").addClass("scroll-left");
+        } 
+        else {
+            $(".card3").removeClass("scroll-left");
         }
     });
 });
